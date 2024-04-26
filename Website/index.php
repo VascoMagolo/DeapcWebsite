7363ae140cfb3dd3e0000c968b253020c9befafa
Vasco Magolo <vascomagolo@gmail.com>
2024-04-26 09:00:12 +0100

    update

---
+++ Website/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <button><a href="login.php">Click to log</a></button>
+    <?php
+    // Your PHP code goes here
+    ?>
+    
+</body>
+</html>
---
+++ Website/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+    <h2>Log</h2>
+    
+    <?php
+    // Your PHP code goes here
+    ?>
+    
+</body>
+</html>
