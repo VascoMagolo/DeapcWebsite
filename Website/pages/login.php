4621f4ee4bc5a298f89e850d1f78a4cd2762c37e
vitormaio <168173279+vitormaio@users.noreply.github.com>
2024-05-24 10:54:49 +0100

    update2
    changed the register page css

--- Website/pages/login.php
+++ Website/pages/login.php
@@ -4,18 +4,54 @@
     include ("../pages/header.php");
 
     
+<<<<<<< Updated upstream
     ?>
+=======
+        /* UPDATE
+        1: correu tudo bem
+        0: erro na QUERY
+        */
+    
+        /* DELETE
+        1: correu tudo bem
+        0: erro na QUERY
+        */
+    
+        /* INSERT
+        id: correu tudo bem
+        0: erro na QUERY
+        */
+        
+        if(isset($result->num_rows)) { // SELECT
+            $arrRes = array();
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $arrRes[] = $row;
+                }
+            }
+            return $arrRes;
+        }
+        else if ($result === TRUE) { // INSERT, DELETE, UPDATE
+            if($last_id = $arrConfig['conn']->insert_id) {
+                return $last_id;
+            }
+            return 1;
+        } 
+        return 0;
+    }
+    
+>>>>>>> Stashed changes
 ?>
 <body>
     <h2>Log</h2>
     <form action="../scripts/login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required>
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required>
-        <button type="submit">Log</button>
-        <p>Don't have an account?</p>
-        <a href="register.php">Register</a>
+        <label class="registertext" for="username">Username:</label>
+        <input class= "textboxes" type="text" name="username" id="username" required>
+        <label class= "registertext" for="password">Password:</label>
+        <input class= "textboxes" type="password" name="password" id="password" required>
+        <button class="registerbutton" type="submit">Log</button>
+        <p><label class="registertext"> Don't have an account?</p>
+        <a class= "registertext" href="register.php">Register</a>
     </form>
     <?php
     // Your PHP code goes here
